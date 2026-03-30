b49f0e989d41a8eafb35483d44f6e8380b8eed93
DhiaElhak-Ezzeddini <dhiaelhak.ezzeddini@supcom.tn>
2026-03-30 10:15:01 +0200

    utils fixed

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from gemma import PaliGemmaConfig,PaliGemmaForConditionalGeneration
+import torch
 from transformers import AutoTokenizer
 import json
 import glob
@@ -15,7 +16,7 @@ def load_hf_model(model_path:str,device:str)->Tuple[PaliGemmaForConditionalGener
     tensors = {}
 
     for file in safetensors_files : 
-        with safe_open(file, framework="pt", device=device) as f:
+        with safe_open(file, framework="pt", device="cpu") as f: ## 
             for key in f.keys():
                 tensors[key] = f.get_tensor(key)
 
@@ -24,8 +25,10 @@ def load_hf_model(model_path:str,device:str)->Tuple[PaliGemmaForConditionalGener
         model_config_file = json.load(f)
         config = PaliGemmaConfig(**model_config_file)
 
-    model = PaliGemmaForConditionalGeneration(config).to(device)
-    model.load_state_dict(tensors,strict=False)
+    model = PaliGemmaForConditionalGeneration(config)
+    #model = model.half() ## trying to avoid OOM errors by loading the model directly in float16,
+    model = model.to(device) 
+    model.load_state_dict(tensors,strict=False) ## strict = False since we might have some extra tensors in the safetensors files that are not part of the model's state dict (e.g. optimizer states, training states, etc.)
     model.tie_weights()
 
     return (model, tokenizer)
